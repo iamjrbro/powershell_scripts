c3fe59c8fef66e9e2c17b8ed21f02a3ca3336295
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2025-07-23 18:19:56 -0300

    Update empty_groups.ps1

--- Microsoft/Azure/empty_groups.ps1
+++ Microsoft/Azure/empty_groups.ps1
@@ -1,3 +1,5 @@
+USANDO MODULO AZURE AD
+
 # Instalar o módulo, se necessário
 # Install-Module AzureAD -Scope CurrentUser
 
@@ -17,6 +19,37 @@ foreach ($group in $groups) {
             DisplayName = $group.DisplayName
             ObjectId    = $group.ObjectId
             Mail        = $group.Mail
+        }
+    }
+}
+
+# Exibir os grupos vazios
+$emptyGroups | Format-Table -AutoSize
+
+
+USANDO GRAPH
+
+# Instalar o módulo, se necessário
+# Install-Module Microsoft.Graph -Scope CurrentUser
+
+# Conectar ao Microsoft Graph
+Connect-MgGraph -Scopes "Group.Read.All"
+
+# Obter todos os grupos
+$allGroups = Get-MgGroup -All
+
+# Inicializar lista de grupos vazios
+$emptyGroups = @()
+
+# Verificar membros de cada grupo
+foreach ($group in $allGroups) {
+    $members = Get-MgGroupMember -GroupId $group.Id -ErrorAction SilentlyContinue
+    if (!$members) {
+        $emptyGroups += [PSCustomObject]@{
+            DisplayName = $group.DisplayName
+            Id          = $group.Id
+            Mail        = $group.Mail
+            GroupType   = $group.GroupTypes -join ', '
         }
     }
 }
