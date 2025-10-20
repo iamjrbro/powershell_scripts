a3a8d118029c021e8d3ae645c1557b471b977db6
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2025-10-20 08:48:28 -0300

    Update restrict_Certificate_Lifetime.ps1

--- Microsoft/365/restrict_Certificate_Lifetime.ps1
+++ Microsoft/365/restrict_Certificate_Lifetime.ps1
@@ -25,5 +25,64 @@ Connect-MgGraph -Scopes "Policy.Read.All","Directory.Read.All"
 # Comando para listar a política
 Get-MgPolicyAuthenticationMethodPolicy | Select-Object -ExpandProperty AuthenticationMethodConfigurations
 
-# Procure pela seção de X509Certificate
+# Desativar  X509Certificate
+
+Update-MgPolicyAuthenticationMethodPolicyAuthenticationMethodConfiguration `
+  -AuthenticationMethodConfigurationId "X509Certificate" `
+  -BodyParameter @{ state = "disabled" }
+
+# Mantendo ativo, mas escopado
+
+Update-MgPolicyAuthenticationMethodPolicyAuthenticationMethodConfiguration `
+  -AuthenticationMethodConfigurationId "X509Certificate" `
+  -BodyParameter @{
+    state = "enabled";
+    includeTargets = @(
+      @{
+        id = "<ObjectID-do-grupo>";
+        targetType = "group";
+        targetedAuthenticationMethod = "x509Certificate"
+      }
+    )
+  }
+
+
+# Vinculando a policy 
+
+Update-MgPolicyAuthenticationMethodPolicyAuthenticationMethodConfiguration `
+  -AuthenticationMethodConfigurationId "X509Certificate" `
+  -BodyParameter @{
+    state = "enabled";
+    includeTargets = @(
+      @{
+        id = "<ObjectId do grupo>";
+        targetType = "group";
+        targetedAuthenticationMethod = "x509Certificate"
+      }
+    )
+  }
+
+
+
+
+# baixando app
+
+# Conectar ao Microsoft Graph
+Connect-MgGraph -Scopes "Application.Read.All"
+
+# Buscar todas as App Registrations
+$apps = Get-MgApplication -All
+
+# Exibir nome e ID no console
+$apps | Select-Object DisplayName, AppId, Id, PublisherDomain, CreatedDateTime | Format-Table
+
+# (Opcional) Exportar para CSV
+$apps | Select-Object DisplayName, AppId, Id, PublisherDomain, CreatedDateTime `
+| Export-Csv -Path "C:\AppRegistrations.csv" -NoTypeInformation -Encoding UTF8
+
+Se quiser ver apenas os apps que usam certificados, você pode filtrar assim:
+
+$apps | Where-Object { $_.KeyCredentials.Count -gt 0 } | 
+Select-Object DisplayName, AppId, @{Name='CertCount';Expression={$_.KeyCredentials.Count}}
+
 
