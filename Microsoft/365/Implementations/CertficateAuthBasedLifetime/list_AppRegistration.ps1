8054fda522ac6bf8eccf30fa0060e4411c616195
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2025-10-22 16:47:03 -0300

    Update list_AppRegistration.ps1

--- Microsoft/365/Implementations/CertficateAuthBasedLifetime/list_AppRegistration.ps1
+++ Microsoft/365/Implementations/CertficateAuthBasedLifetime/list_AppRegistration.ps1
@@ -11,7 +11,7 @@ $apps | Select-Object DisplayName, AppId, Id, PublisherDomain, CreatedDateTime |
 $apps | Select-Object DisplayName, AppId, Id, PublisherDomain, CreatedDateTime `
 | Export-Csv -Path "C:\AppRegistrations.csv" -NoTypeInformation -Encoding UTF8
 
-Se quiser ver apenas os apps que usam certificados, você pode filtrar assim:
+# Se quiser ver apenas os apps que usam certificados, você pode filtrar assim:
 
 $apps | Where-Object { $_.KeyCredentials.Count -gt 0 } | 
 Select-Object DisplayName, AppId, @{Name='CertCount';Expression={$_.KeyCredentials.Count}}
