ae219ec15fe3019126103c3c63fe0a5c38110c4e
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2026-04-17 16:23:49 -0300

    added some codes into the repo

--- Microsoft/Microsoft Places/floor_plan.ps1
+++ Microsoft/Microsoft Places/floor_plan.ps1
@@ -1,5 +1,16 @@
-Install-Module MicrosoftPlaces
+# instalando e conectando os módulos
+
+Connect-ExchangeOnline
+Install-Module MicrosoftPlaces -Force
 Import-Module MicrosoftPlaces
+Connect-MicrosoftPlaces
 Get-Command -Module MicrosoftPlaces *PlaceV3*
 Connect-MicrosoftPlaces
 Get-PlaceV3 -Type Building
+
+# habilitação de funcionalidades do Places
+Set-PlacesSettings -EnablePlacesWebApp $true
+
+# habilita o acesso ao aplicativo web do Places (em Public Preview), permitindo interação com mapas e espaços via navegador
+
+Set-PlacesSettings -EnablePlacesFinder $true
