fb749e48baa60c957109fb3b6515db0899044ea2
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2026-09-14 12:26:45 -0300

    docs: padroniza comentários de defender_Update

--- Microsoft/Defender/defender_Update.ps1
+++ Microsoft/Defender/defender_Update.ps1
@@ -1,5 +1,9 @@
-#run CMD as administrator
+# Execute os comandos em um Prompt de Comando como Administrador.
 
 cd %ProgramFiles%\Windows Defender
+
+# Remove definições dinâmicas existentes do Microsoft Defender.
 MpCmdRun.exe -removedefinitions -dynamicsignatures
+
+# Baixa e instala as assinaturas mais recentes do Microsoft Defender.
 MpCmdRun.exe -SignatureUpdate
