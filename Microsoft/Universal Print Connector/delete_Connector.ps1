203e700c2842872ce802fb4dc9b686ca170e5862
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2026-09-14 12:25:58 -0300

    docs: padroniza comentários de delete_Connector

--- Microsoft/Universal Print Connector/delete_Connector.ps1
+++ Microsoft/Universal Print Connector/delete_Connector.ps1
@@ -1,12 +1,22 @@
+<#
+.SYNOPSIS
+Lista e remove conectores do Microsoft Universal Print.
 
-# instale o modulo UniversalPrintManagement se ainda não estiver instalado
+.DESCRIPTION
+Instala o módulo UniversalPrintManagement, conecta ao serviço, lista os conectores disponíveis e remove o conector informado pelo ID.
+
+.NOTES
+Substitua COLE-O-ID-AQUI pelo ID do conector que deve ser removido.
+#>
+
+# Instala o módulo UniversalPrintManagement, caso ainda não esteja instalado.
 Install-Module UniversalPrintManagement
 
-# conecte-se ao serviço Universal Print
+# Conecta ao serviço Universal Print.
 Connect-UPService
 
-# get a lista de conectores
+# Lista os conectores disponíveis.
 Get-UPConnector
 
-# remove o conector especificado pelo ID
+# Remove o conector especificado pelo ID.
 Remove-UPConnector -ConnectorId "COLE-O-ID-AQUI"
