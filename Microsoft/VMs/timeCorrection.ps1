e8a98472f753a8740af161c09b8a5d403c8d6380
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2026-09-14 12:24:20 -0300

    docs: padroniza comentários de timeCorrection

--- Microsoft/VMs/timeCorrection.ps1
+++ Microsoft/VMs/timeCorrection.ps1
@@ -1,5 +1,6 @@
-# Inicie o PS como admin
-# Set-TimeZone -Id "GMT Standard Time" (se preferir GMT)
+# Execute o PowerShell como Administrador.
+# Para usar outro fuso horário, substitua o ID abaixo pelo fuso desejado.
+# Exemplo: Set-TimeZone -Id "GMT Standard Time"
 
+# Define o fuso horário para o horário de Brasília.
 Set-TimeZone -Id "E. South America Standard Time"
-
