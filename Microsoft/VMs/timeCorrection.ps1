69ad20dc37b9fec0642e028ba814270f44a97427
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2026-09-15 08:30:31 -0300

    chore: remove legacy timezone filename

--- Microsoft/VMs/timeCorrection.ps1
+++
@@ -1,6 +0,0 @@
-# Execute o PowerShell como Administrador.
-# Para usar outro fuso horário, substitua o ID abaixo pelo fuso desejado.
-# Exemplo: Set-TimeZone -Id "GMT Standard Time"
-
-# Define o fuso horário para o horário de Brasília.
-Set-TimeZone -Id "E. South America Standard Time"
