6d596dd9c8b992f2a2f983428ea5010fb1b451cb
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2026-09-15 08:26:06 -0300

    chore: correct Defender script extension

--- Microsoft/Defender/defender_Update.ps1
+++
@@ -1,9 +0,0 @@
-# Execute os comandos em um Prompt de Comando como Administrador.
-
-cd %ProgramFiles%\Windows Defender
-
-# Remove definições dinâmicas existentes do Microsoft Defender.
-MpCmdRun.exe -removedefinitions -dynamicsignatures
-
-# Baixa e instala as assinaturas mais recentes do Microsoft Defender.
-MpCmdRun.exe -SignatureUpdate
