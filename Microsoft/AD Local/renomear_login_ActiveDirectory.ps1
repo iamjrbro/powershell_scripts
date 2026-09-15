1db45aeec209f486608a8f9a2b1a31c84efff449
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2026-09-15 08:28:27 -0300

    chore: remove legacy filename after rename

--- Microsoft/AD Local/renomear_login_ActiveDirectory.ps1
+++
@@ -1,5 +0,0 @@
-# altere o nome do user no AD local | não executar o PS como admin
-
-Install-Module Microsoft.Graph -Scope CurrentUser
-Connect-MgGraph -Scopes "User.ReadWrite.All"
-Update-MgUser -UserId user.upn@domain.com -UserPrincipalName user.upn@domain.com
